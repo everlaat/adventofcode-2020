2b7dbc748afe368f9ec9f9582a20d2dd6fb4a846
Elvin Verlaat <everlaat@gmail.com>
2020-12-01 19:10:06 +1100

    Cleanup tests

--- tests/Days/Day1Test.elm
+++ tests/Days/Day1Test.elm
@@ -32,15 +32,29 @@ simplifiedInput =
 suite : Test
 suite =
     Test.describe "AdventOfCode 2020 Day 1"
-        [ Test.test "D1P1 simplified input" <|
+        [ Test.test "D1P1 no input" <|
+            \_ ->
+                D01.part1 1 ""
+                    |> Expect.equal Nothing
+        , Test.test "D1P1 simplified input" <|
             \_ ->
                 D01.part1 6 simplifiedInput
                     -- 2 + 4 will be found before 3 + 3
-                    |> Expect.equal (Just 8)
+                    |> Expect.equal (Just (2 * 4))
         , Test.test "D1P1 example input" <|
             \_ ->
                 D01.part1 2020 exampleInput
                     |> Expect.equal (Just 514579)
+        , Test.test "D1P2 simplified input (1)" <|
+            \_ ->
+                D01.part2 10 simplifiedInput
+                    -- 2 + 4 + 4 will be found before 2 + 4 + 4
+                    |> Expect.equal (Just (2 * 4 * 4))
+        , Test.test "D1P2 simplified input (11)" <|
+            \_ ->
+                D01.part2 11 simplifiedInput
+                    -- 3 + 4 + 4
+                    |> Expect.equal (Just (3 * 4 * 4))
         , Test.test "D1P2 example input" <|
             \_ ->
                 D01.part2 2020 exampleInput
