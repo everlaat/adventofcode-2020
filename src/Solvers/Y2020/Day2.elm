e455682a22886fbe841c1f03f9161104add9e7da
Elvin Verlaat <everlaat@gmail.com>
2020-12-02 20:10:38 +1100

    Use elm/parser

--- src/Solvers/Y2020/Day2.elm
+++ src/Solvers/Y2020/Day2.elm
@@ -1,6 +1,7 @@
-module Solvers.Y2020.Day2 exposing (part1, part2, solvers)
+module Solvers.Y2020.Day2 exposing (parserPassword, parserPasswords, part1, part2, partToSolver, solvers)
 
 import Com.Solver as Solver exposing (Solver)
+import Parser exposing ((|.), (|=), Parser)
 
 
 solvers : List Solver
@@ -10,52 +11,31 @@ solvers =
     ]
 
 
-partToSolver : (String -> String) -> (String -> Result String String)
+partToSolver : (List Password -> String) -> (String -> Result String String)
 partToSolver f =
-    f >> Ok
+    Parser.run parserPasswords
+        >> Result.map f
+        >> Result.mapError Parser.deadEndsToString
 
 
-inputToList : String -> List String
-inputToList =
-    String.lines >> List.map String.trim
+parseAlphaString : Parser String
+parseAlphaString =
+    Parser.succeed ()
+        |. Parser.chompWhile (\char -> Char.isAlpha char)
+        |> Parser.getChompedString
 
 
-listToPasswords : List String -> List Password
-listToPasswords =
-    List.filterMap
-        (\a ->
-            case String.split " " a of
-                [ range, letter, password ] ->
-                    case
-                        ( String.split "-" range |> List.filterMap String.toInt
-                        , String.replace ":" "" letter |> String.uncons
-                        )
-                    of
-                        ( [ x, y ], Just ( char, _ ) ) ->
-                            Password x y char password
-                                |> Just
-
-                        _ ->
-                            Nothing
-
-                _ ->
-                    Nothing
-        )
-
-
-part1 : String -> String
+part1 : List Password -> String
 part1 =
-    inputToList
-        >> listToPasswords
-        >> List.filter
-            (\{ x, y, char, password } ->
-                let
-                    amount =
-                        String.indexes (String.fromChar char) password
-                            |> List.length
-                in
-                amount >= x && amount <= y
-            )
+    List.filter
+        (\{ x, y, char, password } ->
+            let
+                amount =
+                    String.indexes (String.fromChar char) password
+                        |> List.length
+            in
+            amount >= x && amount <= y
+        )
         >> List.length
         >> String.fromInt
 
@@ -68,24 +48,71 @@ type alias Password =
     }
 
 
-part2 : String -> String
+part2 : List Password -> String
 part2 =
-    inputToList
-        >> listToPasswords
-        >> List.filter
-            (\{ x, y, char, password } ->
-                let
-                    indexes =
-                        String.indexes (String.fromChar char) password
-                            |> List.map ((+) 1)
+    List.filter
+        (\{ x, y, char, password } ->
+            let
+                indexes =
+                    String.indexes (String.fromChar char) password
+                        |> List.map ((+) 1)
 
-                    a =
-                        List.member x indexes
+                a =
+                    List.member x indexes
 
-                    b =
-                        List.member y indexes
-                in
-                a && not b || not a && b
-            )
+                b =
+                    List.member y indexes
+            in
+            a && not b || not a && b
+        )
         >> List.length
         >> String.fromInt
+
+
+
+-- Parsing
+
+
+parserPasswords : Parser (List Password)
+parserPasswords =
+    Parser.loop []
+        (\list ->
+            Parser.oneOf
+                [ Parser.succeed (\password -> Parser.Loop (password :: list))
+                    |= parserPassword
+                    |. Parser.spaces
+                , Parser.succeed ()
+                    |> Parser.map (\_ -> Parser.Done (List.reverse list))
+                ]
+        )
+
+
+parserPassword : Parser Password
+parserPassword =
+    Parser.succeed Password
+        |. Parser.spaces
+        |= Parser.int
+        |. Parser.symbol "-"
+        |. Parser.spaces
+        |= Parser.int
+        |. Parser.spaces
+        |= parseSingleAlphaChar
+        |. Parser.symbol ":"
+        |. Parser.spaces
+        |= parseAlphaString
+
+
+parseSingleAlphaChar : Parser Char
+parseSingleAlphaChar =
+    Parser.succeed ()
+        |. Parser.chompIf (\char -> Char.isAlpha char)
+        |> Parser.getChompedString
+        |> Parser.andThen
+            (\a ->
+                case String.uncons a of
+                    Just ( char, _ ) ->
+                        Parser.succeed char
+
+                    _ ->
+                        Parser.problem "Something went wrong parsing this char"
+            )
--- tests/Solvers/Y2020/Day2Test.elm
+++ tests/Solvers/Y2020/Day2Test.elm
@@ -1,6 +1,7 @@
 module Solvers.Y2020.Day2Test exposing (suite)
 
 import Expect
+import Parser
 import Solvers.Y2020.Day2 as D2
 import Test exposing (Test)
 
@@ -17,10 +18,28 @@ exampleInput =
 suite : Test
 suite =
     Test.describe "AdventOfCode 2020 Day 2"
-        [ Test.test "D2P1 exampleInput" <|
+        [ Test.test "Parse password" <|
             \_ ->
-                D2.part1 exampleInput |> Expect.equal "2"
+                Parser.run D2.parserPassword "1-3 a: abcde"
+                    |> Expect.equal (Ok { x = 1, y = 3, char = 'a', password = "abcde" })
+        , Test.test "Parse passwords" <|
+            \_ ->
+                Parser.run D2.parserPasswords exampleInput
+                    |> Expect.equal
+                        (Ok
+                            [ { x = 1, y = 3, char = 'a', password = "abcde" }
+                            , { x = 1, y = 3, char = 'b', password = "cdefg" }
+                            , { x = 2, y = 9, char = 'c', password = "ccccccccc" }
+                            ]
+                        )
+        , Test.test "D2P1 exampleInput" <|
+            \_ ->
+                exampleInput
+                    |> D2.partToSolver D2.part1
+                    |> Expect.equal (Ok "2")
         , Test.test "D2P2 exampleInput" <|
             \_ ->
-                D2.part2 exampleInput |> Expect.equal "1"
+                exampleInput
+                    |> D2.partToSolver D2.part2
+                    |> Expect.equal (Ok "1")
         ]
