aa1d6911a00a863683490cb2c2084b317b37f88d
Elvin Verlaat <everlaat@gmail.com>
2020-12-03 17:16:31 +1100

    Same approach but cleaned it up a bit

--- src/Solvers/Y2020/Day3.elm
+++ src/Solvers/Y2020/Day3.elm
@@ -17,7 +17,7 @@ type Area
 
 
 type alias Matrix =
-    Array (Array Area)
+    List (Array Area)
 
 
 inputToMatrix : String -> Matrix
@@ -25,110 +25,53 @@ inputToMatrix =
     String.trim
         >> String.lines
         >> List.map
-            (String.split ""
-                >> List.map
+            (String.toList
+                >> List.filterMap
                     (\a ->
-                        if a == "." then
-                            Clear
+                        case a of
+                            '#' ->
+                                Just Tree
 
-                        else
-                            Tree
+                            '.' ->
+                                Just Clear
+
+                            _ ->
+                                Nothing
                     )
                 >> Array.fromList
             )
-        >> Array.fromList
 
 
 partToSolver : (Matrix -> String) -> (String -> Result String String)
 partToSolver f =
-    inputToMatrix
-        >> f
-        >> Ok
+    inputToMatrix >> f >> Ok
 
 
 part1 : Matrix -> String
 part1 matrix =
-    let
-        result =
-            List.repeat (Array.length matrix) Clear
-    in
-    List.indexedMap
-        (\index _ ->
-            let
-                x =
-                    (index + 1) * 3
-
-                y =
-                    (index + 1) * 1
-
-                row =
-                    Array.get y matrix
-
-                cellLength =
-                    Maybe.map Array.length row
-                        |> Maybe.withDefault 1
-
-                cell =
-                    remainderBy cellLength x
-
-                cellContents =
-                    row
-                        |> Maybe.map (\r -> Array.get cell r)
-                        |> Maybe.andThen identity
-                        |> Maybe.withDefault Clear
-            in
-            cellContents
-        )
-        result
-        |> List.filter ((==) Tree)
-        |> List.length
+    getTreesOnPath matrix ( 3, 1 )
         |> String.fromInt
 
 
 part2 : Matrix -> String
 part2 matrix =
-    let
-        result =
-            List.repeat (Array.length matrix) Clear
-
-        moves =
-            [ ( 1, 1 ), ( 3, 1 ), ( 5, 1 ), ( 7, 1 ), ( 1, 2 ) ]
-    in
-    moves
-        |> List.map
-            (\( mx, my ) ->
-                List.indexedMap
-                    (\index _ ->
-                        let
-                            x =
-                                (index + 1) * mx
-
-                            y =
-                                (index + 1) * my
-
-                            row =
-                                Array.get y matrix
-
-                            cellLength =
-                                Maybe.map Array.length row
-                                    |> Maybe.withDefault 1
-
-                            cell =
-                                remainderBy cellLength x
-
-                            cellContents =
-                                row
-                                    |> Maybe.map (\r -> Array.get cell r)
-                                    |> Maybe.andThen identity
-                                    |> Maybe.withDefault Clear
-                        in
-                        cellContents
-                    )
-                    result
-                    |> List.filter ((==) Tree)
-                    |> List.length
-            )
-        |> List.foldl
-            (\a b -> a * b)
-            1
+    [ ( 1, 1 )
+    , ( 3, 1 )
+    , ( 5, 1 )
+    , ( 7, 1 )
+    , ( 1, 2 )
+    ]
+        |> List.map (getTreesOnPath matrix)
+        |> List.foldl (*) 1
         |> String.fromInt
+
+
+getTreesOnPath : Matrix -> ( Int, Int ) -> Int
+getTreesOnPath matrix ( e, s ) =
+    matrix
+        |> List.drop s
+        |> List.indexedMap Tuple.pair
+        |> List.filter (Tuple.first >> (+) 1 >> remainderBy s >> (==) 0)
+        |> List.indexedMap (\i ( _, row ) -> Array.get (remainderBy (Array.length row) ((i + 1) * e)) row)
+        |> List.filter ((==) (Just Tree))
+        |> List.length
